81b9a48c93c46629ee5a3b291ba4a757bd1bb1fe
sarahdiscenza <sarah_discenza@yahoo.com>
2023-05-15 20:35:55 -0400

    make boilerplate and functions

---
+++ script.js
@@ -0,0 +1,16 @@
+function getSum (addend1, addend2) {
+    return addend1 + addend2;
+}
+
+
+function getDifference(number1, number2) {
+    return number1-number2;
+} 
+
+function getProduct(integer1, integer2) {
+    return integer1 * integer2;
+}
+
+function getQuotient(dividend, divisor) {
+    return dividend/divisor;
+}
