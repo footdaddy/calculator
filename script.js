cd2e88df84d566f125c323e33965f8c18603fffe
sarahdiscenza <sarah_discenza@yahoo.com>
2023-05-19 18:05:00 -0400

    made 2 number basic operations

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function getSum (addend1, addend2) {
     return addend1 + addend2;
 }
 
-
 function getDifference(number1, number2) {
     return number1-number2;
 } 
@@ -20,13 +19,13 @@ let num2;
 let operator;
 
 function operate(num1, operator, num2) {
-    if (operator === '+') {
+    if (operator === 'add') {
         getSum(num1, num2);
     }
-    else if (operator === '-') {
+    else if (operator === 'subtract') {
         getDifference(num1, num2);
     }
-    else if (operator === '*') {
+    else if (operator === 'multiply') {
         getProduct(num1, num2);
     }
     else {
@@ -34,3 +33,136 @@ function operate(num1, operator, num2) {
     }
 }
 
+result = document.getElementById('result');
+let clear = document.getElementById('clear');
+let one = document.getElementById('one');
+let two = document.getElementById('two');
+let three = document.getElementById('three');
+let four = document.getElementById('four');
+let five = document.getElementById('five');
+let six = document.getElementById('six');
+let seven = document.getElementById('seven');
+let eight = document.getElementById('eight');
+let nine = document.getElementById('nine');
+let zero = document.getElementById('zero');
+let add = document.getElementById('add');
+let subtract = document.getElementById('subtract');
+let multiply = document.getElementById('multiply');
+let divide = document.getElementById('divide');
+let equals = document.getElementById('equals');
+
+clear.addEventListener("click", () => {
+    result.innerHTML = "";
+    num1 = "0";
+    num2 = "0";
+    operator = "";
+})
+
+one.addEventListener("click", () => {
+    result.innerHTML += "1";
+})
+
+two.addEventListener("click", () => {
+    result.innerHTML += "2";
+})
+
+three.addEventListener("click", () => {
+    result.innerHTML += "3";
+})
+
+four.addEventListener("click", () => {
+    result.innerHTML += "4";
+})
+
+five.addEventListener("click", () => {
+    result.innerHTML += "5";
+})
+
+six.addEventListener("click", () => {
+    result.innerHTML += "6";
+})
+
+seven.addEventListener("click", () => {
+    result.innerHTML += "7";
+})
+
+eight.addEventListener("click", () => {
+    result.innerHTML += "8";
+})
+
+nine.addEventListener("click", () => {
+    result.innerHTML += "9";
+})
+
+zero.addEventListener("click", () => {
+    result.innerHTML += "0";
+})
+
+divide.addEventListener("click", () => {
+    num1 = result.innerHTML;
+    operator = "/";
+    result.innerHTML = num1 + " / ";
+})
+
+add.addEventListener("click", () => {
+    num1 = result.innerHTML;
+    operator = "+";
+    result.innerHTML = num1 + " + ";
+})
+
+multiply.addEventListener("click", () => {
+    num1 = result.innerHTML;
+    operator = "*";
+    result.innerHTML = num1 + " * ";
+})
+
+subtract.addEventListener("click", () => {
+    num1 = result.innerHTML;
+    operator = "-";
+    result.innerHTML = num1 + " - ";
+})
+
+equals.addEventListener("click", () => {
+    display = result.innerHTML.toString();
+    if ((display.includes('+')) || (display.includes('-')) || (display.includes('*')) || (display.includes('/'))) {
+        if (operator === "+") {
+            let num2 = display.slice(display.indexOf(" + " + 4), );
+            if (num2 >= 0) {
+          result.innerHTML=((+num1)+(+num2));
+         }
+             else {
+            result.innerHTML="ERROR";
+         }
+    }
+        else if (operator === "-") {
+            let num2 = display.slice(display.indexOf(" - " + 4), )
+             if (num2 >= 0) {
+            result.innerHTML=((+num1)-(+num2));
+             }
+             else {
+               result.innerHTML="ERROR"
+             }
+            }
+        else if (operator === "*") {
+            let num2 = display.slice(display.indexOf(" * " + 4), )
+            if (num2 >= 0) {
+                result.innerHTML = ((+num1)*(+num2));
+            }
+            else {
+                result.innerHTML="ERROR"
+            }
+        }
+        else {
+            let num2 = display.slice(display.indexOf(" / " + 4), )
+            if ((+num2) > 0) {
+                    result.innerHTML = ((+num1)/(+num2));
+            }
+            else if (((+num1) === 0) && ((+num2) === 0)) {
+                result.innerHTML = "0";
+            }
+            else {
+                result.innerHTML = "undefined";
+            }
+        }
+    }
+})
